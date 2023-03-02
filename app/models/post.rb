d8e67411be41ba12e7d73354631846ff3eec80b6
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-03-02 20:27:37 +0200

    Add unit specs for all models' methods and validations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   after_save :recent_comments
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :comment_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   private
