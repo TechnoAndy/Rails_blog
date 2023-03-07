49a18439ad5b51b701128badcd7f48b452e3f850
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-03-07 15:55:33 +0200

    Add spec tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe PostsController, type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/users/:user_id/posts'
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      get '/users/:user_id/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users/:user_id/posts'
+      expect(response.body).to include('Posts')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get user_posts_path(1, 1) }
+
+    it 'should return a successful response' do
+      expect(response).to have_http_status(:success)
+    end
   end
 end
