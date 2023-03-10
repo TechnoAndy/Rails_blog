8fc2e6e037a16530b2298801ecf8ecdd891eb5ec
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-03-10 23:02:31 +0200

    Add forms, comment and like features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.comment_counter = 0
+    @post.likes_counter = 0
+    @post.author = current_user
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comment_counter, :likes_counter)
+  end
 end
