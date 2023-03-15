2de1a6d1dc314c40f8d050cf5854a6cef071a216
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-03-15 16:59:37 +0200

    Fix linter error

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe '#UserIndex', type: :feature do
         author: @user1, title: 'Hello', text: 'Naturally beautiful', comment_counter: 1, likes_counter: 8
       ),
       @post2 = Post.create(
-        author: @user2, title: 'Hi', text: 'Art is anything you can get away with', comment_counter: 3, 
+        author: @user2, title: 'Hi', text: 'Art is anything you can get away with', comment_counter: 3,
         likes_counter: 28
       )
     ]
