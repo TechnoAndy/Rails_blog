5658e8ef780a23f0c6e81b9c644f6605a5f42dec
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-03-17 00:58:15 +0200

    Chnages made as required by reviewer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
-# frozen_string_literal: true
 
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
+require 'letter_opener'
+
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -21,13 +22,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -43,7 +44,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Default URL for the Devise mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
